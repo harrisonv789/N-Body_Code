a7fc1d67eb043de9ec40dc8b98b122a1e930da53
Harrison Verrios <harrison.v1@live.com>
2021-09-30 12:26:03 +1000

    Adjusted the time class with more functions and better code

--- modules/time.py
+++ modules/time.py
@@ -1,60 +1,119 @@
+# Use numpy for storing the floats as doubles
+from numpy import float64
+
 # Class with some time properties
 class Time:
     
     # Stores the current time
-    time: float = 0
+    time: float64 = 0
 
     # Stores the timestep interval
-    delta: float = 0.01
+    delta: float64 = 0.01
 
     # Stores the starting time
-    start: float = 0
+    start: float64 = 0
 
     # Stores the final time
-    end: float = 1
+    end: float64 = 1.0
 
     # The number of total steps
-    steps_max: 0
+    steps_max: int = 0
 
     # The number of current steps
     steps: int = 0
 
     # Initialiser constructor
-    def __init__ (self, start_time: float = 0.0, end_time: float = 1.0, delta_time: float = 0.01):
+    def __init__ (self, start_time: float64 = 0.0, end_time: float64 = 1.0, delta_time: float64 = 0.01):
         self.start = start_time
         self.end = end_time
         self.delta = delta_time
         self.reset()
 
-    # Function that resets the time
+    ##########################################################################
+
+
+
+    ##########################################################################
+    # CLOCK FUNCTIONS
+    ##########################################################################
+
+    # Resets the clock back to the start time
     def reset (self):
         self.time = self.start
         self.steps = 0
         self.steps_max = int((self.end - self.start) / self.delta)
 
-    # Function that increments the time
+    # Increments the time by the delta time
     def increment (self):
         self.time += self.delta
         self.steps += 1
 
+    # Increases the time by a certain amount of steps (+=)
+    def __iadd__ (self, other: int):
+        self.time += self.delta * int(other)
+        self.steps += int(other)
+        return self
+
     # Returns the progress as a fraction from 0 to 1
     @property
-    def progress (self):
+    def progress (self) -> float64:
         return (self.time - self.start) / (self.end - self.start)
 
     # Function that returns whether or not the time is running
     @property
-    def running (self):
+    def running (self) -> bool:
         return self.time <= self.end
 
+    ##########################################################################
+
+
+
+    ##########################################################################
+    # EQUALITY FUNCTIONS
+    ##########################################################################
+
+    # Equality operator
+    def __eq__(self, other: object) -> bool:
+        return self.time == other.time
+
+    # Inequality operator
+    def __ne__(self, other: object) -> bool:
+        return self.time != other.time
+
+    # Greater than operator
+    def __gt__(self, other: object) -> bool:
+        return self.time > other.time
+    
+    # Greater than or equal to operator
+    def __ge__(self, other: object) -> bool:
+        return self.time >= other.time
+
+    # Less than operator
+    def __lt__(self, other: object) -> bool:
+        return self.time < other.time
+    
+    # Less than or equal to operator
+    def __le__(self, other: object) -> bool:
+        return self.time <= other.time
+
+    ##########################################################################
+
+
+
+    ##########################################################################
+    # CONVERSION FUNCTIONS
+    ##########################################################################
+
     # Returns the value of the time
-    def __call__ (self):
+    def __call__ (self) -> float64:
         return self.time
 
     # Returns the float value of the time
-    def __float__ (self):
+    def __float__ (self) -> float:
         return self.time
 
     # Returns the string form
-    def __str__ (self):
+    def __str__ (self) -> str:
         return "Time: %8.4f,  Start: %8.4f,  End: %8.4f,  Delta: %8.4f" % (self.time, self.start, self.end, self.delta)
+
+    ##########################################################################
